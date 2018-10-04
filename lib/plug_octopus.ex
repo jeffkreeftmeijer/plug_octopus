32b10c4af5d667f15b8611a6040238eb00f28277
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2018-10-04 12:01:40 +0200

    Add Router, show Octopus

--- lib/plug_octopus.ex
+++ lib/plug_octopus.ex
@@ -1,18 +1,73 @@
 defmodule Plug.Octopus do
-  @moduledoc """
-  Documentation for Plug.Octopus.
-  """
+  use Plug.Router
+  use Plug.Debugger
 
-  @doc """
-  Hello world.
+  @octopus ~S'''
+                        ___
+                     .-'   `'.
+                    /         \
+                    |         ;
+                    |         |           ___.--,
+           _.._     |0) ~ (0) |    _.---'`__.-( (_.
+    __.--'`_.. '.__.\    '--. \_.-' ,.--'`     `""`
+   ( ,.--'`   ',__ /./;   ;, '.__.'`    __
+   _`) )  .---.__.' / |   |\   \__..--""  """--.,_
+  `---' .'.''-._.-'`_./  /\ '.  \ _.-~~~````~~~-._`-.__.'
+        | |  .' _.-' |  |  \  \  '.               `~---`
+         \ \/ .'     \  \   '. '-._)
+          \/ /        \  \    `=.__`~-.
+     jgs  / /\         `) )    / / `"".`\
+    , _.-'.'\ \        / /    ( (     / /
+     `--~`   ) )    .-'.'      '.'.  | (
+            (/`    ( (`          ) )  '-;
+             `      '-;         (-'
+  '''
 
-  ## Examples
+  @flipped ~S'''
+                                ___
+                             .'`   '-.
+                            /         \
+                            ;         |
+          ,--.___           |         |
+        ._) )-.__`'---._    | (0) ~ (0|     _.._
+        `""`     `'--., '-._/ .--'    /.__.' .._`'--.__
+                 __    `'.__.' ,;   ;\.\ __,'   `'--., )
+         _,.--"""  ""--..__/   /|   | \ '.__.---.  ( (`_
+  '.__.-`_.-~~~````~~~-._ /  .' /\  \._`'-._.-''.'. '---`
+   `---~`               .'  /  /  |  | '-._ '.  | |
+                       (_.-' .'   /  /     '. \/ /
+                    .-~`__.=`    /  /        \ \/
+     jgs          /`.""` \ \    ( (`         /\ \
+                  \ \     ) )    \ \        / /'.'-._ ,
+                   ) |  .'.'      '.'-.    ( (   `~--`
+                  ;-'  ( (          `) )    `\)
+                        '-)         ;-'      `
+  '''
 
-      iex> Plug.Octopus.hello()
-      :world
+  plug(:match)
+  plug(:dispatch)
 
-  """
-  def hello do
-    :world
+  match _ do
+    case Plug.Conn.fetch_query_params(conn) do
+      %{query_params: %{"flip" => "crash"}} ->
+        raise "Octopus crashed"
+
+      %{query_params: params} = conn ->
+        conn
+        |> put_resp_content_type("text/html")
+        |> send_resp(200, body(params["flip"]))
+    end
+  end
+
+  defp body("right"), do: body(@flipped, "/?flip=left")
+  defp body(_), do: body(@octopus, "/?flip=right")
+
+  defp body(octopus, href) do
+    """
+    <title>Octolicious!</title>
+    <pre>#{octopus}</pre>
+    <p><a href="#{href}">flip!</a></p>
+    <p><a href="/?flip=crash">crash!</a></p>
+    """
   end
 end
